97d83c50f9d472995163dc8118943c36e1e992c8
johnamii <81453889+johnamii@users.noreply.github.com>
2022-09-27 16:23:52 -0700

    set to WIP website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Header = () => {
+
+  return  (
+    <div className='header'>
+      Waiting to find some time to build my personal website, here's a ditto to pass the time
+      <br/>
+      <a href='https://github.com/johnamii'> github </a> ---
+      <a href='https://www.linkedin.com/in/john-flanagan-343058230/'> linkedin </a>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className='body'>
+      BODY
+    </div>
+  )
+}
+
+const HomeScreen = () => {
+  return (
+    <div className='homescreen'>
+      <Header/>
+      <div className='center'>
+        <div className='ditto-div' >
+          <img src='ditto haha.gif' style={{height: '75%', width: '75%'}}/>
+        </div>
+      </div>
+      
+      
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomeScreen/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
