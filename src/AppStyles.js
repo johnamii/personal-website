61309a0d91a64e5bdabf050006da1be93f5a2e51
johnamii <81453889+johnamii@users.noreply.github.com>
2022-12-05 14:59:07 -0800

    structured constant elements

---
+++ src/AppStyles.js
@@ -0,0 +1,50 @@
+
+const AppStyles = {
+    color: {
+        light : {
+            primary: "",
+            primaryGradient: "",
+            secondary: "",
+            text: ""
+        },
+        dark: {
+            primary: "",
+            primaryGradient: "",
+            secondary: "",
+            text: ""
+        },
+        sidebar: {
+            background: "",
+            accent: "",
+            border: "",
+            text: ""
+        },
+        popup: {
+            background: "",
+            accent: "",
+            text: ""
+        }
+    },
+    font: {
+        title: {
+
+        },
+        paragraph: {
+            color: 'black'
+        }
+    },
+    icon: {
+        size: {
+            height: 30,
+            width: 30
+        },
+        images: {
+            logo: '/sailboat.png',
+            sun: "",
+            moon: "",
+            poketheme: '/pokeballs.png',
+            terni: ""
+        }
+    }
+}
+export default AppStyles;
---
+++ src/texts.js
@@ -0,0 +1,15 @@
+
+const texts = {
+    about_me: {
+        who: {
+            description: "I'm a Colorado native studying computer science at UCLA."
+        },
+        interests: {
+            description: "I like dogs, music, and soccer."
+        },
+        this_page: {
+            description: "I made this page using React.js and JavaScript"
+        }
+    }
+}
+export default texts;
