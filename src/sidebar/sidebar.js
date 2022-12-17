31e6d3bd1c1f6b437a7c677af6d8520bce95d40e
johnamii <81453889+johnamii@users.noreply.github.com>
2022-12-17 15:22:19 -0700

    fixed sidebar

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,33 +1,21 @@
 import React, { useState } from 'react'
-import './App.css'
-import AppStyles from './AppStyles'
+import {AppStyles} from '../AppStyles'
+import './sidebar.css'
+
+  export const IconButton = (props) => {
   
-  const IconButton = (props) => {
-  
-  //props.extended
     return (
       <a href={props.url}>
-        <button className='sidebar-icon' 
+        <button className='sidebar-icon'
         href='https://github.com/johnamii' 
-        style={props.ext ? {width: '25vh'} : {width:'5vh'}}
+        style={props.ext ? {width: '12rem'} : {width:'2.5rem'}}
         onClick={(val) => { props.onClick && props.onClick(val)}}
         >
-          <img src={props.img} alt={props.name} style={{height: '70%'}}/>
-          {props.ext ? props.name : ''}
+          <img src={props.img} alt={props.name} className='icon-image'/>
+          {props.ext && props.name}
         </button>
       </a>
-    )
-    
-  }
-  
-  export const MobileBar = (props) => {
-    return (
-      <div className='mobile-bar'>
-        <IconButton name='GitHub' url='https://github.com/johnamii' img='/github-light.png' />
-        <IconButton name='PokeTheme Battles' url='https://poketheme.johnamii.com' img={AppStyles.icon.images.poketheme} />
-        <IconButton name='Terni Lapilli' url='https://terni.johnamii.com' img='/pebbles.png'/>
-      </div>
-    )
+    ) 
   }
   
   export const SideBar = (props) => {
@@ -37,16 +25,18 @@ import AppStyles from './AppStyles'
       <div className='sidebar' 
       onMouseEnter={() => setExtended(true)} 
       onMouseLeave={() => setExtended(false)} 
-      style={extended ? {width:'15%', boxShadow: '2px 0px 10px 2px rgba(0,0,0,.5)'} : {width: '4%'}}
+      style={extended ? {boxShadow: '2px 0px 10px 2px rgba(0,0,0,.5)'} : {}}
       >
         <div className='sidebar-title' style={extended ? {fontSize:'large'} : {}}> <b> Me </b></div>
         
-        <IconButton name='About Me' img='/sailboat.png' ext={extended} onClick={(val) => {props.popupClick('about-me')}}/>
+        <IconButton name='About Me' img={AppStyles.icon.images.logoDark} ext={extended} onClick={(val) => {props.popupClick('about-me')}}/>
         <IconButton name='GitHub' url='https://github.com/johnamii' img='/github-light.png' ext={extended}/>
   
         <div className='divider-bar'/>
   
         <div className='sidebar-title' style={extended ? {fontSize:'large'} : {}}> <b> Projects </b></div>
+
+        <IconButton name ='Scootly.io' url="https://github.com/scootly/react-native-client" img='/scootly.png' ext={extended}/>
         <IconButton name='PokeTheme Battles' url='https://poketheme.johnamii.com' img='/pokeballs.png' ext={extended}/>
         <IconButton name='Terni Lapilli' url='https://terni.johnamii.com' img='/pebbles.png' ext={extended}/>
 
